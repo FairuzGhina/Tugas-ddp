a9029a58ef66ab2b3970b33b644b4e88b16f9426
FairuzGhina <fairuzgame39@gmail.com>
2024-12-19 07:54:36 +0700

    app.py

---
+++ pertemuan 13/app.py
@@ -0,0 +1,14 @@
+import stremlit as st 
+
+st.write('hello world!')
+
+dashboard = st.Page("./pages/dashboard.py", title="Dashboard")
+nabung = st.Page("./pages/nabung.py", title="Nabung")
+
+pg = st.navigation({
+    "Dashboard": [dashboard],
+    "Nabung": [nabung],
+})
+if "Nabung" not in st.session_state:
+    st.session_state["Nabung"] = []
+pg.run()
